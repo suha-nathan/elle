75309c314b73331c186adc7dc97632882931cd88
suhanathan <suhasini_95@hotmail.com>
2021-04-13 23:14:20 +0800

    dashboard charts added

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Row, Col } from "react-bootstrap"
+import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend, Pie, PieChart } from "recharts"
+import radarData from "../data/radardata"
+import pieData from "../data/pieData"
+
+import MyCalendar from "./MyCalendar"
 
 function Dashboard() {
     return (
         <div>
-            <h3>Hi Suha you've got 99 points so far!</h3>
-            Charts go here
+            <h3 className="header-text" >Hi Suha you've got 99 points so far!</h3>
+            <Row className="justify-content-md-center" >
+                <Col className="p-4" >
+                    <RadarChart className="chart-div" outerRadius={90} width={300} height={250} data={radarData}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="subject" />
+                        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+                        <Radar name="Mike" dataKey="A" stroke="#1C5085" fill="#1C5085" fillOpacity={0.4} />
+                        {/* <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} /> */}
+                        {/* <Legend /> */}
+                    </RadarChart>
+                </Col>
+
+                <Col className="p-4" >
+                    <PieChart width={250} height={250}>
+                        <Pie data={pieData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#5787AB" label />
+                        <Legend/>
+                    </PieChart>               
+                </Col>
+
+                <Col className="m-4 p-4" md="auto">
+                    <MyCalendar/>
+                </Col>
+
+                <Col >
+                </Col>
+
+                
+            </Row>
+            
+            
         </div>
     )
 }
---
+++ client/src/components/MyCalendar.js
@@ -0,0 +1,19 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import eventData from '../data/eventData'
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const MyCalendar = props => (
+    <Calendar
+      localizer={localizer}
+      events={eventData}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+)
+
+export default MyCalendar
