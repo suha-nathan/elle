781f491f12ae02fc849abb7670d2a95b74c8be62
suhanathan <suhasini_95@hotmail.com>
2021-04-13 19:04:50 +0800

    init basic layout SPA

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Row, Col, Image, Navbar } from 'react-bootstrap'
+import SideNavbar from './SideNavbar'
 
-export default function Layout() {
+
+export default function Layout({ children }) {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Row className="vw-100">
+                <Col md={2} >
+                    <SideNavbar/>
+                </Col>
+
+                <Col>
+                <Navbar>
+                    {/* <Navbar.Brand >Hello Mark</Navbar.Brand> */}
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                        {/* <h2> Hello Suha </h2>
+                        Signed in as: <a href="#login">Mark Otto</a> */}
+                        </Navbar.Text>
+                        <Image src="https://via.placeholder.com/80" roundedCircle />
+                    </Navbar.Collapse>
+                </Navbar>
+                    {children}
+                </Col>
+            </Row>
+        </>
     )
 }
---
+++ client/src/components/SideNavbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsChatDots, BsPerson, BsLayers } from "react-icons/bs"
+import { AiOutlineHome } from "react-icons/ai"
+import { IoGameControllerOutline, IoSettingsOutline } from "react-icons/io5"
+
+import Elephant from "./Elephant"
+
+
+export default function SideNavbar() {
+    const [sidebar, setSideBar] = React.useState(true)
+
+    return (
+        <nav className ="nav flex-column vh-100 " style={{backgroundColor:'#99BFD3'}} >
+            <div className="mx-auto p-2 m-2">
+                <h3><Elephant/> Elle </h3>
+            </div>
+            
+            <Link className="nav-link" to="/dashboard"> <AiOutlineHome/> Dashboard</Link>
+            <Link class="nav-link" to="/my-courses"> <BsLayers/> My Courses</Link>
+            <Link class="nav-link" to="/chat"> <BsChatDots/> Messages/Chat </Link>
+            <Link class="nav-link" to="/games"> <IoGameControllerOutline/> Games</Link>
+            <Link class="nav-link" to="/my-profile"> <BsPerson/> My Profile</Link>
+            <Link class="nav-link" to="/settings"> <IoSettingsOutline/> Settings</Link>
+        </nav>
+    )
+}
+
