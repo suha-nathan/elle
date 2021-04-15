dab2fbf486f852fd471a237a94cb4b522c3c413a
suhanathan <suhasini_95@hotmail.com>
2021-04-16 04:07:11 +0800

    folder structure change for deploy

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const mongoStore = require('connect-mongo')
 const passport = require('passport')
-
-const methodOverride = require('method-override')
+const path = require('path')
 
 
 require('./lib/passport.config')
 
+// app.use(express.static(path.join(__dirname, 'client/build')));
+if(process.env.NODE_ENV === 'production') {  
+    app.use(express.static(path.join(__dirname, 'client/build'))); 
+ //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
+}
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -24,7 +28,7 @@ app.use(session({
     secret: process.env.SECRET,
     resave:false,
     saveUninitialized:true,
-    store:mongoStore.create({mongoUrl:process.env.DB})
+    store:mongoStore.create({mongoUrl:process.env.PROD_DB})
 }))
 
 app.use(passport.initialize())
@@ -38,9 +42,12 @@ app.use('/account', require("./routes/auth.route"))
 app.use("/",passport.authenticate('jwt', { session: false }), require("./routes/course.route"))
 app.use("/",passport.authenticate('jwt', { session: false }), require("./routes/user.route"))
 
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
 app.use((req, res) => {
     res.status(404).json({ message:"route not found"})
 })
+
+
 
 app.listen(process.env.PORT, () =>{console.log(`listening on port ${process.env.PORT}`)})
