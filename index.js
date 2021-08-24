3ee3172baf5fdbd735e5f4b44a8d7ce7cfe6f4a9
Padmanathan Suhasini <suhanathan@mtpa-0000.investcloud.int>
2021-08-25 00:31:55 +0800

    testing commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const logger = require('morgan')
+const logger = require('morgan')
 // const socketio = require('socket.io')()
 // const WebSockets = require('./lib/WebSockets')
 // //connect to mongoDB 
